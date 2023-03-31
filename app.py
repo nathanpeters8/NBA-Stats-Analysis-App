a10802e134cb39bf9c538790bb4f74b14a672227
Nate Peters <peters.nate8@gmail.com>
2023-03-31 14:06:00 -0600

    added app.py file with imports

---
+++ app.py
@@ -0,0 +1,4 @@
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import scoreboard
+from nba_api.stats.static import teams
+from nba_api.stats.static import players
